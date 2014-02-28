96c2690618bdcf63eb7f7971e589a40f59937404
weger <lovexctk@gmail.com>
2014-03-01 00:49:48 +0800

    rename

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
-DU
+Bui
 ==========
 
 
-DU是一个通用模块化前端开发平台，提供了常用的项目构建、调试、优化等一系列开发时的支持,只为提升开发效率。
+Bui是一个通用模块化前端开发平台，提供了常用的项目构建、调试、优化等一系列开发时的支持,只为提升开发效率。
 
 
 安装与更新
 -------
 
-edp已经发布到npm上，可以通过下面的npm命令安装。`-g`选项是必须选项，使用`-g`全局安装后，可以获得command line的`edp`命令。在Linux/Mac平台下，全局安装可能需要`sudo`。
+bui已经发布到npm上，可以通过下面的npm命令安装。`-g`选项是必须选项，使用`-g`全局安装后，可以获得command line的`bui`命令。在Linux/Mac平台下，全局安装可能需要`sudo`。
 
-    $ [sudo] npm install -g du
+    $ [sudo] npm install -g bui
 
-如果想要升级当前edp的版本，请运行如下命令。在Linux/Mac平台下，升级可能需要`sudo`。
+如果想要升级当前bui的版本，请运行如下命令。在Linux/Mac平台下，升级可能需要`sudo`。
 
-    $ [sudo] npm update -g du
+    $ [sudo] npm update -g bui
 
 
 使用
@@ -22,8 +22,8 @@ edp已经发布到npm上，可以通过下面的npm命令安装。`-g`选项是�
 
 我们推荐通过命令行的方式使用edp。直接命令行下执行edp将显示可以调用的命令，包含内建命令和用户定制的命令。
 
-    $ du
-    Usage: du <command>
+    $ bui
+    Usage: bui <command>
 
     Builtin Commands:
 
--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-var du = require('../lib/commander'),
+var bui = require('../lib/commander'),
     install = require('../lib/install'),
     webserver = require('../lib/webserver'),
     release = require('../lib/release'),
@@ -9,25 +9,25 @@ var du = require('../lib/commander'),
 //colors 全局设置所处可用
 colors.setTheme({silly:"rainbow",input:'grey',verbose:'cyan',prompt:'grey',info:'green',data:'grey',help:'cyan',warn:'yellow',debug:'blue',error:'red'});
 // 初始化项目信息
-du.command('install')
+bui.command('install')
    .description(' - 新建一个项目,需要参数为新项目名称')
    .action(install);
 
-du.command("tplcombo")
+bui.command("tplcombo")
    .description(" - 用于开发时合并模板文件")
    .option('-w,--watch','监听文件变化，及时合并')
    .action(webserver);
 
-du.command("server")
+bui.command("server")
    .description(" - 用于开发时调试的WebServer" + "http://localhost:8888".warn)
    .option('-s,--port','启动端口号')
    .action(webserver);
 
-du.command('release')
+bui.command('release')
    .description(" - 打包压缩优化所有文件，加版本戳防缓存，发布项目")
    .option('-d,--dest','发布目录，相对于当前的')
    .action(release);
 
-du.version(require('../package').version);
-du.parse(process.argv);
+bui.version(require('../package').version);
+bui.parse(process.argv);
 
