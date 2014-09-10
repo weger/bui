713bce8a3bc54020ba8948520e4a2dc0176a3cf7
fengshuo <fengshuo142003@gmail.com>
2014-09-11 03:37:46 +0800

    change fork repo, update files

--- bin/cli.js
+++ bin/cli.js
@@ -5,6 +5,7 @@ var bui = require('../lib/commander'),
     webserver = require('../lib/webserver'),
     release = require('../lib/release'),
     tplcombo = require('../lib/tplcombo'),
+    less2css = require('../lib/less2css')
     colors = require('../lib/colors');
 
 //colors 全局设置所处可用
@@ -16,6 +17,7 @@ bui.command('install')
 
 bui.command("tplcombo")
    .description(" - 用于开发时合并模板文件")
+   .option('-i,--i18n','支持国际化')
    .option('-w,--watch','监听文件变化，及时合并')
    .action(tplcombo);
 
@@ -23,6 +25,11 @@ bui.command("server")
    .description(" - 用于开发时调试的WebServer,默认地址：" + "http://localhost:8888".warn)
    .option('-p,--port','启动端口号')
    .action(webserver);
+
+bui.command("less2css")
+    .description(" - 用于转换less文件至css")
+    .action(less2css);
+
 
 bui.command('release')
    .description(" - 打包压缩优化所有文件，加版本戳防缓存，发布项目")
